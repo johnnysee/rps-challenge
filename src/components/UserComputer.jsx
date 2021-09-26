c67f4ed588459aea19d3a251c4a209367e8f0530
johnnysee <js.johnny.see@gmail.com>
2021-09-26 22:01:14 +0200

    Added useEffect and display computer's value and comparison to check winner

--- src/components/UserComputer.jsx
+++ src/components/UserComputer.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const UserComputer = () => {
   const [userChoice, setUserChoice] = useState(null);
-  const options = ['rock', 'paper', 'scissors']
-  const [computerValue, setComputerValue] = useState(null)
+  const options = ["rock", "paper", "scissors"];
+  const [computerValue, setComputerValue] = useState(null);
+  const [comparison, setComparison] = useState(null);
 
   const fetchComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random()*options.length)
-    setComputerValue(options[randomNumber])
-  }
+    const randomNumber = Math.floor(Math.random() * options.length);
+    setComputerValue(options[randomNumber]);
+
+  };
+
+  useEffect(() => {
+    switch (userChoice + computerValue) {
+      case "rockscissors":
+      case "paperrock":
+      case "scissorspaper":
+        setComparison("User wins!");
+        break;
+      case "scissorsrock":
+      case "paperscissors":
+      case "rockpaper":
+        setComparison("User lose!");
+        break;
+      case "scissorsscissors":
+      case "paperpaper":
+      case "rockrock":
+        setComparison("It is a draw!");
+        break;
+        default:
+    }
+
+  }, [userChoice, computerValue])
 
   const handleClick = (value) => {
     setUserChoice(value);
+    fetchComputerChoice();
   };
 
   return (
     <div>
       <h2 data-cy="user">User's choose: {userChoice}</h2>
-      {options.map(choice => <button data-cy="scissors" onClick={() => handleClick(choice)}>{choice}</button>)}
-      <h2 data-cy="computer">Computer</h2>
-      <p>Hi</p>
+      {options.map((choice, index) => (
+        <button key={index} data-cy="scissors" onClick={() => handleClick(choice)}>
+          {choice}
+        </button>
+      ))}
+      <h2 data-cy="computer">Computer choose: {computerValue}</h2>
+      <h2>{comparison}</h2>
     </div>
   );
 };
