f4398fdf6722e6bb9b1b2d166340c4397a2a95fb
johnnysee <js.johnny.see@gmail.com>
2021-09-26 17:45:37 +0200

    Added test for 3 buttons with rock, paper and scissors - test passing

--- cypress/integration/user_visits_application_spec.js
+++ cypress/integration/user_visits_application_spec.js
@@ -3,16 +3,28 @@ describe("users can visit application", () => {
     cy.visit('/')
   })
 
-  it ('is expected true to be true', () => {
+  it ('is expected header to be RPS', () => {
     cy.get('[data-cy=header]').should('contain', 'Rock - Paper - Scissors')
   })
 
-  it ('is expected true to be true', () => {
+  it ('is expected to display User', () => {
     cy.get('[data-cy=user]').should('contain', 'User')
   })
 
-  it ('is expected true to be true', () => {
+  it ('is expected to display Computer', () => {
     cy.get('[data-cy=computer]').should('contain', 'Computer')
+  })
+
+  it ('is expected to display a button 1', () => {
+    cy.get('[data-cy=rock]').click()
+  })
+
+  it ('is expected to display a button 2', () => {
+    cy.get('[data-cy=paper]').click()
+  })
+
+  it ('is expected to display a button 3', () => {
+    cy.get('[data-cy=scissor]').click()
   })
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
     <div>
       <h1 data-cy="header">Rock - Paper - Scissors</h1>
       <h2 data-cy="user">User</h2>
+      <button data-cy="rock">Rock</button>
+      <button data-cy="paper">Paper</button>
+      <button data-cy="scissor">Scissor</button>
       <h2 data-cy="computer">Computer</h2>
     </div>
   )
